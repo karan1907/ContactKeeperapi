6b7532248cbc8e4799fdb58809927901799a032e
Goldy King <goldyking@Goldys-MacBook-Pro.local>
2019-11-07 16:44:39 +0530

    Finished the whole app functionality

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -37,7 +37,7 @@ const ContactForm = () => {
       updateContact(contact);
     }
 
-    setContact({ name: "", email: "", phone: "", type: "personal" });
+    clearAll();
   };
 
   const clearAll = () => {
---
+++ client/src/context/Alert/AlertState.js
@@ -0,0 +1,33 @@
+import React, { useReducer } from "react";
+import uuid from "uuid";
+import AlertContext from "../Alert/alertContext";
+import alertReducer from "../Alert/alertReducer";
+import { SET_ALERT, REMOVE_ALERT } from "../types";
+
+const AlertState = props => {
+  const initialState = [];
+  const [state, dispatch] = useReducer(alertReducer, initialState);
+
+  // Set Alert
+  const setAlert = (msg, type, timewout = 5000) => {
+    const id = uuid.v4();
+    dispatch({
+      type: SET_ALERT,
+      payload: { msg, type, id }
+    });
+    setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timewout);
+  };
+
+  return (
+    <AlertContext.Provider
+      value={{
+        alerts: state,
+        setAlert
+      }}
+    >
+      {props.children}
+    </AlertContext.Provider>
+  );
+};
+
+export default AlertState;
